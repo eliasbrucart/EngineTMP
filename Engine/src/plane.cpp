4a86724933b920fb13e729621d39fbab95bfb521
eliasbrucart <eliasbrucart@hardgames.com.ar>
2022-08-30 22:06:21 -0300

    Added plane class

---
+++ Engine/src/plane.cpp
@@ -0,0 +1,38 @@
+#include "plane.h"
+
+using namespace Engine;
+
+Plane::Plane() {
+	_normal = glm::vec3(0.0f, 1.0f, 0.0f);
+	_distance = 0.0f;
+}
+
+Plane::Plane(glm::vec3 normal, float distance) {
+	_normal = normal;
+	_distance = distance;
+}
+
+Plane::Plane(const glm::vec3& p1, const glm::vec3& normal) {
+	_normal = glm::normalize(normal);
+	_distance = glm::dot(_normal, p1);
+}
+
+Plane::~Plane() {
+
+}
+
+void Plane::SetNormal(glm::vec3 normal) {
+	_normal = normal;
+}
+
+void Plane::SetDistance(float distance) {
+	_distance = distance;
+}
+
+glm::vec3 Plane::GetNormal() {
+	return _normal;
+}
+
+float Plane::GetDistance() {
+	return _distance;
+}
---
+++ Engine/src/plane.h
@@ -0,0 +1,26 @@
+#ifndef PLANE_H
+#define PLANE_H
+#include "export.h"
+#include "glm.hpp"
+#include "vec3.hpp"
+#include <iostream>
+using namespace std;
+
+namespace Engine {
+	class ENGINE_API Plane {
+	private:
+		glm::vec3 _normal;
+		float _distance;
+	public:
+		Plane();
+		Plane(glm::vec3 normal, float distance);
+		Plane(const glm::vec3& p1, const glm::vec3& normal);
+		~Plane();
+		void SetNormal(glm::vec3 normal);
+		void SetDistance(float distance);
+		glm::vec3 GetNormal();
+		float GetDistance();
+	};
+}
+
+#endif
